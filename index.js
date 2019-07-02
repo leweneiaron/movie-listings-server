4cbe31c93eb2f3e30def1eb87bf6d1543a878b3c
Lewenei Aron <leweneiaron@gmail.com>
2019-07-02 19:18:40 +0300

    add get /api/food endpoint

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "foo",
+    password: "password",
+    database: "fast_foods"
+});
 
-app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+app.get("/api/food", (req, res) => {
+    pool.query("SELECT * FROM fast_foods.food", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+
+        res.json(rows);
+    })
+});
 
 app.listen(9000, () => console.log("App listening on port 9000"));
